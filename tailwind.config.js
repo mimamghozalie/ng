956e5b3a24496eb06e2d3ae3ef08218321b86298
Ryan Jan Borja <ryanjan18@outlook.ph>
2020-11-23 13:48:16 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.html', './src/**/*.ts']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer')
+  ],
+}
